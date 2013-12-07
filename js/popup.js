82a085efd7a7f0cecb5e082949cf99a0ad7721c0
Sem <sembrestels@riseup.net>
2013-12-07 10:18:26 +0100

    Improved way to open the wallet.

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var page = document.getElementById('history');
-  page.addEventListener('click', function (e) {
-    chrome.tabs.create({url:chrome.extension.getURL("wallet.html")});
-    //chrome.tabs.executeScript(null,
-    //  {code:"document.body.style.backgroundColor='red'"});
-    window.close();
-  });
-});
-
 function PopupCtrl($scope) {
   $scope.identities = [
     {id: 'gengix'},
