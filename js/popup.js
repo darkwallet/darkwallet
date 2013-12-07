5307756fb91d0085ef45a1ed237a228fd1eb2d9c
Sem <sembrestels@riseup.net>
2013-12-07 08:49:07 +0100

    Added some angularjs behavior to popup.

--- js/popup.js
+++ js/popup.js
@@ -8,3 +8,27 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+function PopupCtrl($scope) {
+  $scope.identities = [
+    {id: 'gengix'},
+    {id: 'sem'},
+  ];
+  
+  $scope.identityChange = function() {
+    if ($scope.identity.id == 'gengix') {
+      $scope.activity = [
+        {action: 'Received bitcoins', amount: '0.05', timestamp: '5 minutes ago'},
+        {action: 'Received bitcoins', amount: '0.01', timestamp: '6 minutes ago'},
+      ];
+    } else {
+      $scope.activity = [
+        {action: 'Received bitcoins', amount: '0.1', timestamp: '15 minutes ago'},
+        {action: 'Received bitcoins', amount: '2', timestamp: '26 minutes ago'},
+      ];
+    }
+  }
+
+  $scope.identity = $scope.identities[0];
+  $scope.identityChange();
+}
+
