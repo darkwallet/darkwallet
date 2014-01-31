fe27b3243d7c319c9830cc9630c7e386559b8c73
Pablo Martin <caedes@sindominio.net>
2014-01-31 10:04:36 +0100

    Document the DarkWallet object.

--- js/darkwallet.js
+++ js/darkwallet.js
@@ -1,3 +1,9 @@
+/*
+ * @fileOverview Main object for generic dark wallet api.
+ */
+
+// DarkWallet object.
 var DarkWallet = {};
 
+// Identity key ring. Holds all identities.
 DarkWallet.keyRing = new IdentityKeyRing();
