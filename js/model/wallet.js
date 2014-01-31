a9c85b77df2ef7d6243898e1efebca97d3244324
Pablo Martin <caedes@sindominio.net>
2014-01-31 02:30:04 +0100

    check mpk on wallet.

--- js/model/wallet.js
+++ js/model/wallet.js
@@ -6,6 +6,10 @@
 function Wallet(store) {
     this.is_cold = store.get('is_cold');
     this.pubKeys = store.get('pubKeys');
+    this.mpk = store.get('mpk');
+    if (!this.mpk) {
+         console.log("Wallet without mpk!", this.mpk);
+    }
     this.store = store;
 }
 
@@ -16,8 +20,7 @@ Wallet.prototype.getAddress = function(n, is_change) {
     }
     else {
         // derive from mpk
-        var mpk = this.store.mpk;
-        var mpKey = new Bitcoin.BIP32key(mpk);
+        var mpKey = new Bitcoin.BIP32key(this.mpk);
 
         // BIP32 js support is still missing some part and we can't get addresses
         // from pubkey yet, unless we do it custom like here...:
