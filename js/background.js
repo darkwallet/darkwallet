d6b6d15a92d3a7b5b46f4d0bf82c30cd53d846cc
Pablo Martin <caedes@sindominio.net>
2014-02-22 12:42:54 +0100

    namespacing the wallet background service.

--- js/background.js
+++ js/background.js
@@ -1,147 +1,160 @@
-
-var keyRing = new IdentityKeyRing();
-var obeliskClient = new ObeliskClient();
-var currentIdentity = 0;
-
-var identityNames = [];
-
-var connected = false;
-
-var currentHeight = 0;
-
-/***************************************
-/* Identities
+/*
+ * @fileOverview Background service running for the wallet
  */
 
-// Load identity names
-keyRing.loadIdentities(function(names) {
-    if (!names) {
-       console.log("bad loading");
-       return;
-    }
-    // get the first identity
-    //keyRing.get(names[0], loadIdentity);
-});
+function DarkWalletService() {
+    var keyRing = new IdentityKeyRing();
+    var obeliskClient = new ObeliskClient();
+    var currentIdentity = 0;
 
-var getAvailableIdentities = function() {
-    return keyRing.availableIdentities;
-};
+    var identityNames = [];
 
-var loadIdentity = function(idx, userCallback) {
-    var name = keyRing.availableIdentities[idx];
-    console.log("load", name);
-    keyRing.get(name, function(identity) {
-        identity.history.update = function() { sendInternalMessage({name: 'guiUpdate'}); };
-        userCallback(identity);
+    var connected = false;
+
+    var currentHeight = 0;
+
+    /***************************************
+    /* Identities
+     */
+
+    // Load identity names
+    keyRing.loadIdentities(function(names) {
+        if (!names) {
+           console.log("bad loading");
+           return;
+        }
+        // get the first identity
+        //keyRing.get(names[0], loadIdentity);
     });
-}
 
-// Get an identity from the keyring
-var getIdentity = function(idx, loadIdentity) {
-    var identity = keyRing.availableIdentities[idx];
-    return keyRing.identities[identity];
+    this.loadIdentity = function(idx, userCallback) {
+        var name = keyRing.availableIdentities[idx];
+        console.log("load", name);
+        keyRing.get(name, function(identity) {
+            identity.history.update = function() { sendInternalMessage({name: 'guiUpdate'}); };
+            userCallback(identity);
+        });
+    }
 
+    // Get an identity from the keyring
+    this.getIdentity = function(idx, loadIdentity) {
+        var identity = keyRing.availableIdentities[idx];
+        return keyRing.identities[identity];
+
+    }
+    /***************************************
+    /* History and address subscription
+     */
+    function historyFetched(err, walletAddress, history) {
+        if (err) {
+            console.log("Error fetching history for", walletAddress.address);
+            return;
+        }
+        var client = obeliskClient.client;
+        var identity = this.getIdentity(0);
+
+        // pass to the wallet to process outputs
+        identity.wallet.processHistory(walletAddress.address, history);
+
+        // now subscribe the address for notifications
+        client.subscribe(walletAddress.address, function(err, res) {
+            console.log("subscribed", walletAddress.address, err, res);
+
+            // fill history after subscribing to ensure we got all histories already (for now).
+            identity.history.fillHistory(history);
+        }, function(addressUpdate) {
+            console.log("update", addressUpdate)
+        });
+        sendInternalMessage({name: "balanceUpdate"});
+    }
+    // Start up history for an address
+    this.initAddress = function(walletAddress) {
+        var client = obeliskClient.client;
+        var identity = this.getIdentity(0);
+        client.fetch_history(walletAddress.address, function(err, res) { historyFetched(err, walletAddress, res); });
+        if (walletAddress.history) {
+            identity.history.fillHistory(walletAddress.history)
+        }
+    }
+
+    // Handle initial connection to obelisk
+    function handleHeight(err, height) {
+        currentHeight = height;
+        // sendInternalMessage({name: "height", value: height});
+        console.log("height fetched", height);
+    }
+
+    function handleInitialConnect() {
+        var client = obeliskClient.getClient();
+        client.fetch_last_height(handleHeight);
+
+        // get balance for addresses
+        var identity = this.getIdentity(0);
+        Object.keys(identity.wallet.pubKeys).forEach(function(pubKeyIndex) {
+            var walletAddress = identity.wallet.pubKeys[pubKeyIndex];
+            if (walletAddress.index.length > 1) {
+                this.initAddress(walletAddress);
+            }
+        });
+    }
+
+    /***************************************
+    /* Global communications
+     */
+
+    this.connect = function(userCallback) {
+        console.log("connect");
+        if (connected) {
+            userCallback()
+        } else {
+            obeliskClient.connect('ws://85.25.198.97:8888', function() {
+                handleInitialConnect();
+                userCallback();
+            });
+            connected = true;
+        }
+    }
+    this.getKeyRing = function() {
+        return keyRing;
+    }
+
+    this.getClient = function() {
+        return obeliskClient.client;
+    }
 }
 
 /***************************************
 /* Communications
  */
-var sendInternalMessage = function(msg) {chrome.runtime.sendMessage(chrome.runtime.id, msg)};
+var sendInternalMessage = function(msg) {
+    chrome.runtime.sendMessage(chrome.runtime.id, msg)
+};
 
 var addListener = function(callback) {
     chrome.runtime.onMessage.addListener(callback);
 };
 
-/***************************************
-/* History and address subscription
- */
-function historyFetched(err, walletAddress, history) {
-    var client = obeliskClient.client;
-    var identity = getIdentity(0);
-
-    // pass to the wallet to process outputs
-    identity.wallet.processHistory(walletAddress.address, history);
-
-    // now subscribe the address for notifications
-    client.subscribe(walletAddress.address, function(err, res) {
-        console.log("subscribed", walletAddress.address, err, res);
-
-        // fill history after subscribing to ensure we got all histories already (for now).
-        identity.history.fillHistory(history);
-    }, function(addressUpdate) {
-        console.log("update", addressUpdate)
-    });
-    sendInternalMessage({name: "balanceUpdate"});
-}
-
-// Start up history for an address
-function initAddress(walletAddress) {
-    var client = obeliskClient.client;
-    var identity = getIdentity(0);
-    client.fetch_history(walletAddress.address, function(err, res) { historyFetched(err, walletAddress, res); });
-    if (walletAddress.history) {
-        identity.history.fillHistory(walletAddress.history)
-    }
-}
 
 /***************************************
-/* Global communications
+/* Service instance that will be running in the background page
  */
-
-// Handle initial connection to obelisk
-function heightFetched(err, height) {
-    currentHeight = height;
-    // sendInternalMessage({name: "height", value: height});
-    console.log("height fetched", height);
-}
-
-function handleConnect() {
-    var client = obeliskClient.getClient();
-    client.fetch_last_height(heightFetched);
-
-    // get balance for addresses
-    var identity = getIdentity(0);
-    Object.keys(identity.wallet.pubKeys).forEach(function(pubKeyIndex) {
-        var walletAddress = identity.wallet.pubKeys[pubKeyIndex];
-        if (walletAddress.index.length > 1) {
-            initAddress(walletAddress);
-        }
-    });
-}
-var connect = function(userCallback) {
-    console.log("connect");
-    if (connected) {
-        userCallback()
-    } else {
-        obeliskClient.connect('ws://85.25.198.97:8888', function() {
-            handleConnect(); userCallback();
-        });
-        connected = true;
-    }
-}
+var service = new DarkWalletService();
 
 
 /***************************************
 /* Bindings for the page window so we can have easy access
  */
 
-window.connect = connect;
+window.connect = service.connect;
 
-window.loadIdentity = loadIdentity;
-window.getIdentity = getIdentity;
-window.getAvailableIdentities = getAvailableIdentities;
+window.loadIdentity = service.loadIdentity;
+window.getIdentity = service.getIdentity;
 
-window.getKeyRing = function() {
-    return keyRing;
-}
+window.getKeyRing = service.getKeyRing;
 
-window.getClient = function() {
-    return obeliskClient.client;
-}
+window.getClient = service.getClient;
 
-window.initAddress = initAddress;
+window.initAddress = function(_w) {return service.initAddress(_w)};
 
 window.addListener = addListener
 window.sendInternalMessage = sendInternalMessage;
-
-
