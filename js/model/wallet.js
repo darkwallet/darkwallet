1b4855b66366470a092bacd133261cd5e0a70fae
Pablo Martin <caedes@sindominio.net>
2014-02-26 12:32:49 +0100

    change private keys encryption to 256 bits (wallet restore needed)

--- js/model/wallet.js
+++ js/model/wallet.js
@@ -102,7 +102,7 @@ Wallet.prototype.getPrivateKey = function(seq, password, callback) {
 Wallet.prototype.getPrivateData = function(password) {
     var SHA256 = Bitcoin.Crypto.SHA256;
     var passwordDigest = SHA256(SHA256(SHA256( password )));
-    var data = JSON.parse(sjcl.decrypt(passwordDigest, this.store.get('private')));
+    var data = JSON.parse(sjcl.decrypt(passwordDigest, this.store.get('private'), {ks: 256}));
     if (!data.privKeys) {
         data.privKeys = {};
     }
