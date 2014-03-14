35189fe481eee76d70e401a14934aa4d3c94dee3
tilthz <tiltillou@gmail.com>
2014-03-14 02:31:56 +0000

    new theme

--- js/app.js
+++ js/app.js
@@ -19,3 +19,32 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/settings', {templateUrl: 'partials/settings.html', controller: 'WalletSettingsCtrl'});
   $routeProvider.otherwise({redirectTo: '/history'});
 }]);
+
+var app = (function() {
+
+  'use strict';
+  var privateVariable = 'app fired!',
+    docElem = document.documentElement;
+
+  return {
+    publicFunction: function() {
+      console.log(privateVariable);
+    },
+    userAgentInit: function() {
+      docElem.setAttribute('data-useragent', navigator.userAgent);
+    }
+  };
+
+})();
+
+(function() {
+
+  'use strict';
+
+  //foundation init
+  $(document).foundation();
+
+  app.publicFunction();
+  app.userAgentInit();
+
+})();
