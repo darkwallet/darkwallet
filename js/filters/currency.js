f6d700a37ba7f9f9bc15cfbbe5c24f458abf96e8
Pablo Martin <caedes@sindominio.net>
2014-03-17 04:32:47 +0100

    format as bitcoins not millis

--- js/filters/currency.js
+++ js/filters/currency.js
@@ -1,7 +1,7 @@
 define(['./module', 'darkwallet'], function (filters, DarkWallet) {
 filters.filter('currencyFilter', function() {
   return function(input) {
-    return input / 100000;
+    return input / 100000000;
   };
 });
 });
