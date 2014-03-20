57dfd1f318de54c1d81df1b552d86e716ee06eb9
Pablo Martin <caedes@sindominio.net>
2014-03-20 09:30:30 +0100

    show balance for identities in the identity selector.

--- js/controllers/identities.js
+++ js/controllers/identities.js
@@ -1,5 +1,14 @@
-define(['./module'], function (controllers) {
+define(['./module', 'util/services', 'darkwallet'], function (controllers, Services, DarkWallet) {
   'use strict';
   controllers.controller('IdentitiesCtrl', ['$scope', function($scope) {
+    Services.connectNg('wallet', $scope, function(data) {
+      console.log("wallet bus message", data);
+      if (data.type == 'ready') {
+        // identity is ready here
+        console.log('loaded', data.identity)
+        $scope.identities = DarkWallet.getKeyRing().identities;
+      }
+    })
+
   }]);
 });
