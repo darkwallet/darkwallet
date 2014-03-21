8452570a30e4ad399bb7c92e5de33fc1e7f8caed
Sem <sembrestels@riseup.net>
2014-03-21 04:33:22 +0100

    Applied QR directly instead of template an svg.

--- js/frontend/directives/qr.js
+++ js/frontend/directives/qr.js
@@ -2,9 +2,8 @@ define(['./module', 'qrcodejs'], function (directives, QRCode) {
   directives.directive('qr', function() {
     return {
       restrict: 'E',
-      template: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"></svg>',
       link: function(scope, element, attrs) {
-        var qrcode = new QRCode(element[0].firstChild, {
+        var qrcode = new QRCode(element[0], {
           width : attrs.width,
           height : attrs.height,
           correctLevel : QRCode.CorrectLevel.H,
