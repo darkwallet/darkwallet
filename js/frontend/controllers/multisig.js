1d9290ea126a7d5ebcd152ae1f9d0f353a935a1c
Pablo Martin <caedes@sindominio.net>
2014-03-21 08:40:30 +0100

    Add code to generate the multisig fund and read addresses provided by the user in different formats.

--- js/frontend/controllers/multisig.js
+++ js/frontend/controllers/multisig.js
@@ -1,19 +1,29 @@
-define(['./module', 'bitcoinjs-lib'], function (controllers, Bitcoin) {
+define(['./module', 'util/btc'],
+function (controllers, BtcUtils) {
   'use strict';
   controllers.controller('MultisigCtrl', ['$scope', function($scope) {
     $scope.multisig = {};
     $scope.multisig.participants = [];
+    $scope.nSignatures = 2;
+    $scope.multiSigName = '';
 	
     $scope.addParticipant = function(data, vars) {
-      vars.participants.push({address: data});
+        vars = vars || $scope.multisig;
+        vars.participants.push({address: data});
     };
 
     $scope.createMultisig = function() {
-      var participants = [];
-      $scope.multisig.participants.forEach(function(participant) {
-        participants = participant.address;
-      });
-      var script = Bitcoin.Script.createMultiSigOutputScript($scope.numsig, participants);
+        var participants = [];
+        $scope.multisig.participants.forEach(function(participant) {
+            console.log(participant.address.length)
+            participants.push(BtcUtils.decodeAddress(participant.address));
+        });
+        var multisig = BtcUtils.multiSig($scope.nSignatures, participants);
+        multisig.name = $scope.multiSigName;
+        multisig.participants = $scope.multisig.participants.slice(0);
+
+        // Show fund address
+        $scope.multiSigAddress = multisig.address;
     };
   }]);
 });
---
+++ js/util/btc.js
@@ -0,0 +1,64 @@
+define(['bitcoinjs-lib', 'util/stealth'], function(Bitcoin, Stealth) {
+  var convert = Bitcoin.convert;
+  var BtcUtils = {
+    /*
+     * Start a multisig structure out of participant public keys and m
+     */
+    multiSig: function(m, participants){
+        // Create script
+        var script = Bitcoin.Script.createMultiSigOutputScript(m, participants);
+        // Hash for address
+        var hashed = Bitcoin.Util.sha256ripe160(script.buffer);
+        // Encode in base58, v0x05 is multisig
+        var address = Bitcoin.base58.checkEncode(hashed, 0x05);
+        // Encoded script
+        var scriptHex = convert.bytesToHex(script.buffer);
+        return {address: address, script: scriptHex, m: m, pubKeys: participants};
+    },
+
+    /*
+     *  Uncompress a public address
+     */
+    uncompressAddress: function(bytes) {
+        // XXX Untested!
+        var key = Bitcoin.ECPubKey(bytes, true);
+        return key.toBytes(false);
+    },
+
+    /*
+     * Decode an address from string to bytes
+     * Supports the following formats:
+     *  - uncompressed hex
+     *  - compressed hex
+     *  - stealth (S...)
+     *  - mpk (xpub...)
+     */
+    decodeAddress: function(address) {
+        if (address.length == 130) {
+            // Hex uncompressed address
+            bytes = convert.hexToBytes(address)
+        } else if (address.length == 66) {
+            // Hex compressed address
+            bytes = convert.hexToBytes(address);
+        } else if (address.length == 58 && address[0] == 'S') {
+            // Stealth address
+            bytes = Bitcoin.base58.checkDecode(address);
+            bytes = bytes.slice(1,bytes.length-5);
+        } else if (address.length == 111 && address.slice(0,4) == 'xpub') {
+            // Master public key
+            var mpKey = Bitcoin.HDWallet.fromBase58(address);
+            bytes = mpKey.pub.toBytes(false);
+        } else {
+            // Unknown
+            throw Error("Can't decode address for multisig with length " + address.length);
+        }
+        // Decompress if needed
+        if (bytes.length == 32) {
+            bytes = BtcUtils.uncompressAddress(bytes);
+        }
+        return bytes;
+    }
+  }
+
+  return BtcUtils;
+});
