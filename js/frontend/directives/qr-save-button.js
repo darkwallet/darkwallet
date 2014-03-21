7bfae1db155df35955cc54a6c3f7254d235deb78
Sem <sembrestels@riseup.net>
2014-03-21 06:12:26 +0100

    Added download QR button.

---
+++ js/frontend/directives/qr-save-button.js
@@ -0,0 +1,22 @@
+define(['./module', 'qrcodejs'], function (directives, QRCode) {
+  directives.directive('qrSaveButton', function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var div = document.createElement('div');
+        var qrcode = new QRCode(div, {
+          width : attrs.width,
+          height : attrs.height,
+          correctLevel : QRCode.CorrectLevel.H,
+          useSVG: true,
+        });
+        qrcode.makeCode(attrs.data);
+        div.firstChild.setAttribute('width', '400');
+        div.firstChild.setAttribute('height', '400');
+        element[0].setAttribute('href','data:image/svg+xml;charset=utf-8,' + 
+          encodeURIComponent(div.innerHTML));
+        element[0].setAttribute('download', attrs.data + '.svg');
+      }
+    };
+  });
+});
