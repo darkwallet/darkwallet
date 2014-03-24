3b5a4de0cfc12015cdbd35127a19c7a257ce8082
Pablo Martin <caedes@sindominio.net>
2014-03-24 07:17:09 +0100

    Make the currency filters respect the bitcoin unit selection.

--- js/frontend/filters/currency.js
+++ js/frontend/filters/currency.js
@@ -1,14 +1,34 @@
 define(['./module', 'darkwallet'], function (filters, DarkWallet) {
+
+// Convert input to btc units
+var getAsBtc = function(currency, input) {
+  if (currency == 'mbtc') {
+    return input / 100000;
+  } else {
+    return input / 100000000;
+  }
+}
+
+// Filter for presenting a satoshi amount into selected btc unit
 filters.filter('currencyFilter', function() {
   return function(input) {
-    return input / 100000000;
+    var identity = DarkWallet.getIdentity();
+    return getAsBtc(identity.settings.currency, input);
   };
 });
+
+// Filter for presenting a satoshi amount into selected btc unit with unit label
 filters.filter('currencyPresenter', function() {
   return function(input) {
+    var identity = DarkWallet.getIdentity();
     var walletService = DarkWallet.service().getWalletService();
     var rate = walletService.rates[walletService.currency];
-    var formatted = input / 100000000 + "mBTC";
+    var formatted = getAsBtc(identity.settings.currency, input)
+    if (identity.settings.currency == 'mbtc') {
+      formatted += ' mBTC';
+    } else {
+      formatted += ' BTC';
+    }
 
     if (rate) {
       var converted = (input * rate / 100000000).toFixed(2);
