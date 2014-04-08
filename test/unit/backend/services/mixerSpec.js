a2a70ada3e83dc3b7869c5b547f40bfaa5cf942b
Pablo Martin <caedes@sindominio.net>
2014-04-08 17:25:50 +0200

    Add spec files for mixer and ticker services.

---
+++ test/unit/backend/services/mixerSpec.js
@@ -0,0 +1,7 @@
+/*
+ * @fileOverview Background service running for the wallet
+ */
+define(['backend/services/mixer'], function(MixerService) {
+  'use strict';
+});
+
---
+++ test/unit/backend/services/tickerSpec.js
@@ -0,0 +1,7 @@
+/*
+ * @fileOverview Background service running for the wallet
+ */
+define(['backend/services/ticker'], function(TickerService) {
+  'use strict';
+});
+
