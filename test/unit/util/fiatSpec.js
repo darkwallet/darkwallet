e0a34542f01c3e0b1dba7283e44e1e4011c14826
Sem <sembrestels@riseup.net>
2014-04-08 18:17:17 +0200

    Added fiat currencies data unit test.

--- test/unit/util/fiatSpec.js
+++ test/unit/util/fiatSpec.js
@@ -1,3 +1,48 @@
-define(['util/fiat'], function(FiatUtils) {
+define(['util/fiat'], function(fiatCurrencies) {
   'use strict';
+  
+  describe('Fiat Currencies data', function() {
+    
+    var areAllDefined = function(key) {
+      var currency;
+      for(currency in fiatCurrencies) {
+        expect(fiatCurrencies[currency][key]).toBeDefined();
+      }
+    };
+    
+    var areAllTruthy = function(key) {
+      var currency;
+      for(currency in fiatCurrencies) {
+        expect(fiatCurrencies[currency][key]).toBeTruthy();
+      }
+    };
+    
+    it('has all symbols defined', function() {
+      areAllTruthy('symbol');
+    });
+    
+    it('has all names defined', function() {
+      areAllTruthy('name');
+    });
+    
+    it('has all native symbols defined', function() {
+      areAllTruthy('symbol_native');
+    });
+    
+    it('has all decimals digits defined', function() {
+      areAllDefined('decimal_digits');
+    });
+    
+    it('has all roundings defined', function() {
+      areAllDefined('rounding');
+    });
+    
+    it('has all codes defined', function() {
+      areAllTruthy('code');
+    });
+    
+    it('has all plural names defined', function() {
+      areAllTruthy('name_plural');
+    });
+  })
 });
