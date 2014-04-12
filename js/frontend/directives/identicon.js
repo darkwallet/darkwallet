c481500f61217a3d8828e19f02248712478ceb10
Pablo Martin <caedes@sindominio.net>
2014-04-12 13:59:12 +0200

    Optimize a bit.

--- js/frontend/directives/identicon.js
+++ js/frontend/directives/identicon.js
@@ -10,16 +10,16 @@ define(['./module', 'identicon', 'bitcoinjs-lib'], function (directives, Identic
         var iconSize = scope.iconSize || 32;
 
         // Create the identicon
-        function createFromBytes(dataBytes) {
-          var data = new Identicon(Bitcoin.convert.bytesToHex(dataBytes), iconSize).toString();
+        function createFromHex(dataHex) {
+          var data = new Identicon(dataHex, iconSize).toString();
           element.html('<img class="identicon" src="data:image/png;base64,' + data + '">');
         }
         // Watch for hash changes
         scope.$watch('hash', function() {
           if (scope.hash) {
-            var pubKeyBytes = Bitcoin.convert.hexToBytes(scope.hash);
             // take 11 bytes: 22 hex - 60 bit shape, 28 bit color
-            createFromBytes(pubKeyBytes.slice(8,19));
+            // skip first 8 bytes to avoid headers (the input can be arbitrary hex)
+            createFromHex(scope.hash.substr(16, 38));
           }
         })
       }
