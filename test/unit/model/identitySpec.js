229d590b3f846a2754f877bd5e814aa513e48220
Pablo Martin <caedes@sindominio.net>
2014-04-13 17:41:07 +0200

    fix identity spec for new defaults.

--- test/unit/model/identitySpec.js
+++ test/unit/model/identitySpec.js
@@ -37,7 +37,7 @@ define(['model/identity'], function(Identity) {
 
     it('is created properly', function() {
       expect(identity.name).toBe("Sean's Outpost");
-      expect(identity.settings).toEqual({currency: 'BTC', fiatCurrency: 'EUR'});
+      expect(identity.settings).toEqual({currency: 'BTC', fiatCurrency: 'EUR', notifications: {popup: true}});
       expect(identity.store).toBeDefined();
       expect(identity.wallet).toBeDefined();
       expect(identity.txdb).toBeDefined();
