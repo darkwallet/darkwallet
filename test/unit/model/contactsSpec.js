8ecb8a4b893041c14c41ef532b67344fb47bd9b2
Pablo Martin <caedes@sindominio.net>
2014-04-21 12:22:52 +0200

    Add some missing coverage for contacts model.

--- test/unit/model/contactsSpec.js
+++ test/unit/model/contactsSpec.js
@@ -40,10 +40,14 @@ define(['model/contacts'], function(Contacts) {
 
      it('finds a contact', function() {
       var libBitcoin =  {name: 'libbitcoin', address: '04cf2e5b02d6f02340f5a9defbbf710c388b8451c82145b1419fe9696837b1cdefc569a2a79baa6da2f747c3b25a102a081dfd5e799abc41262103e0d17114770b'};
+      var libBitcoin2 =  {name: 'libbitcoin2', address: '04cf2e5b02d6f02340f5a9defbbf710c388b8451c82145b1419fe9696837b1cdefc569a2a79baa6da2f747c3b25a102a081dfd5e799abc41262103e0d17114770b'};
       var libBitcoinPub = [4,207,46,91,2,214,240,35,64,245,169,222,251,191,113,12,56,139,132,81,200,
                            33,69,177,65,159,233,105,104,55,177,205,239,197,105,162,167,155,170,109,162,
                            247,71,195,178,90,16,42,8,29,253,94,121,154,188,65,38,33,3,224,209,113,20,119,11];
+      contacts.addContact(libBitcoin2);
       contacts.addContact(libBitcoin);
+      // this is to trigger an error that should not break the search
+      contacts.contacts[0].pubKeys[0].data = 'foo';
       var libBitcoinShort = [3,207,46,91,2,214,240,35,64,245,169,222,251,191,113,12,56,139,132,81,200,33,69,177,65,159,233,105,104,55,177,205,239];
 
       var contact = contacts.findByPubKey(libBitcoinPub);
@@ -59,6 +63,18 @@ define(['model/contacts'], function(Contacts) {
     
     // it('initializes contacts'); // TODO Delete it in DarkWallet 1.0
 
+    it('generates a contact hash', function() {
+      var data = '04cf2e5b02d6f02340f5a9defbbf710c388b8451c82145b1419fe9696837b1cdefc569a2a79baa6da2f747c3b25a102a081dfd5e799abc41262103e0d17114770b';
+      expect(contacts.generateContactHash(data)).toBe("1e3e2b5cb607bcd53de71e3b2c62dc165cf9ddae9c22b894e3f23166f29b9130");
+    });
+
+    it('throws updating a wrong index', function() {
+      contacts.addContact(satoshiForestNew);
+      contacts.contacts[0].pubKeys = [];
+      expect(function() {
+         contacts.updateKey(contacts.contacts[0], data, 3);
+      }).toThrow();
+    });
     it('updates contact hash', function() {
       contacts.updateContactHash(satoshiForest);
       expect(satoshiForest.hash).toBe('ca308ce5eeda89f8a7607f4a3106eb4a3a52eddf84933b03afb8e1bc0799ecf3');
@@ -113,4 +129,66 @@ define(['model/contacts'], function(Contacts) {
 
     });
   });
+
+  /**
+   * Model upgrade original version, just wipes out the store
+   */
+  describe('Model upgrade 1', function() {
+    var contacts;
+    var store = {
+      init: function(key, value) {
+        if (key == 'contacts') {
+            return {'foo': 'bar'};
+        }
+        return value;
+      },
+      save: function() {
+      },
+      set: function() {
+      }
+    };
+ 
+    beforeEach(function() {
+      contacts = new Contacts(store);
+    });
+
+    it('it upgrades from array properly', function() {
+      expect(contacts.store).toBe(store);
+      expect(contacts.contacts).toEqual([]);
+    });
+
+  });
+
+  /**
+   * Model upgrade from single address to array of address/keys.
+   */
+  describe('Model upgrade 2', function() {
+    var contacts;
+    var satoshiForestNew = {name: 'Satoshi Forest', address: '1M72Sfpbz1BPpXFHz9m3CdqATR44Jvaydd'};
+    var satoshiForest = {name: 'Satoshi Forest', mainKey: 0, pubKeys: [{data: '1M72Sfpbz1BPpXFHz9m3CdqATR44Jvaydd', type: 'address', address: '1M72Sfpbz1BPpXFHz9m3CdqATR44Jvaydd'}]};
+    var store = {
+      init: function(key, value) {
+        if (key == 'contacts') {
+            return [satoshiForestNew, undefined];
+        }
+        return value;
+      },
+      save: function() {
+      },
+      set: function() {
+      }
+    };
+ 
+    beforeEach(function() {
+      contacts = new Contacts(store);
+    });
+
+    it('it upgrades from array properly', function() {
+      expect(contacts.store).toBe(store);
+      expect(contacts.contacts).toEqual([satoshiForestNew]);
+    });
+
+  });
+
+
 });
