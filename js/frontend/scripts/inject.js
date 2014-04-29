5f49cc257a2434296224fb4998c8743b776713fe
Sem <sembrestels@riseup.net>
2014-04-29 19:19:33 +0200

    Added support for bitcoin: URI scheme.

--- js/frontend/scripts/inject.js
+++ js/frontend/scripts/inject.js
@@ -13,3 +13,19 @@ btn.appendChild(t);
 document.body.appendChild(btn);
 */
 
+/*
+ * We don't use registerProtocolHandler() because we have to declare web accessible
+ * resources, and that exposes to the web that the user is using that extension.
+ * 
+ * The following commit is just for documentation, it is not in our source.
+ * https://github.com/sembrestels/darkwallet/commit/b99acd91#diff-4b1eb3dc4
+ * 
+ * Instead, we listen the click event on links that have bitcoin uris.
+ */
+document.body.addEventListener('click', function(e) {
+  var elem = e.target;
+  if (elem.tagName == 'A' && typeof elem.href == 'string' && elem.href.startsWith('bitcoin:')) {
+    window.open('chrome-extension://' + chrome.runtime.id + "/html/index.html#/send?uri=" + encodeURIComponent(elem.href));
+    e.preventDefault();
+  }
+}, false);
