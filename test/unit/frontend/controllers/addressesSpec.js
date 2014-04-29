491fb9ed5c16a2db7f939e28a31889a0eb216217
Pablo Martin <caedes@sindominio.net>
2014-04-29 18:29:19 +0200

    Proper naming for addresses arrays.

--- test/unit/frontend/controllers/addressesSpec.js
+++ test/unit/frontend/controllers/addressesSpec.js
@@ -73,8 +73,8 @@ describe('Addresses controller', function() {
     });
     
     it('generates an address', function() {
-      scope.address = [];
-      scope.allAddress = [];
+      scope.addresses = [];
+      scope.allAddresses = [];
       scope.generateAddress(1, 5);
       expect(wallet.generateAddress).toHaveBeenCalledWith(1, 5);
     });
