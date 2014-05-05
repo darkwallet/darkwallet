89dda48fbae814b3bb9b8428093582ed9977c26e
Pablo Martin (wdp) <caedes@sindominio.net>
2014-05-05 20:49:29 +0200

    Move the shuffleArray function inside the module.

--- js/util/coinjoin.js
+++ js/util/coinjoin.js
@@ -1,20 +1,20 @@
 'use strict';
 
-/**
- * Randomize array element order in-place.
- * Using Fisher-Yates shuffle algorithm.
- */
-function shuffleArray(array) {
+define(['bitcoinjs-lib', 'util/btc'], function(Bitcoin, BtcUtils) {
+
+  /**
+   * Randomize array element order in-place.
+   * Using Fisher-Yates shuffle algorithm.
+   */
+  function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+       array[i] = array[j];
+       array[j] = temp;
     }
     return array;
-}
-
-define(['bitcoinjs-lib', 'util/btc'], function(Bitcoin, BtcUtils) {
+  }
 
   /*
    * CoinJoin Class
