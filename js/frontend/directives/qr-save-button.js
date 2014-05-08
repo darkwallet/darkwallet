15d1037859f80650dcd7f63273c06ee032333bac
Sem <sembrestels@riseup.net>
2014-05-08 22:35:38 +0200

    Save QRs as png.

--- js/frontend/directives/qr-save-button.js
+++ js/frontend/directives/qr-save-button.js
@@ -1,24 +1,24 @@
 'use strict';
 
 define(['./module', 'qrcodejs'], function (directives, QRCode) {
-  directives.directive('qrSaveButton', ['$window', function($window) {
+  directives.directive('qrSaveButton', ['$window', '$timeout', function($window, $timeout) {
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
         var div = $window.document.createElement('div');
         var qrcode = new QRCode(div, {
-          width : attrs.width,
-          height : attrs.height,
-          correctLevel : QRCode.CorrectLevel.H,
-          useSVG: true
+          width : attrs.width || 400,
+          height : attrs.height || 400,
+          correctLevel : QRCode.CorrectLevel.H
         });
         qrcode.makeCode(attrs.data);
-        div.firstChild.setAttribute('width', '400');
-        div.firstChild.setAttribute('height', '400');
-        element[0].setAttribute('href','data:image/svg+xml;charset=utf-8,' + 
-          encodeURIComponent(div.innerHTML));
-        element[0].setAttribute('download', attrs.data + '.svg');
+        $timeout(function() {
+          var data = div.lastChild.getAttribute('src');
+          element[0].setAttribute('href', data);
+          element[0].setAttribute('download', attrs.data + '.png');
+        }, 1);
       }
+      
     };
   }]);
 });
