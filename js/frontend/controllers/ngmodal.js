62651b62532e5b621fddf332878f8dd7acdf1197
Pablo Martin (wdp) <caedes@sindominio.net>
2014-06-15 04:26:47 +0200

    Don't remove the modal content while hiding it.

--- js/frontend/controllers/ngmodal.js
+++ js/frontend/controllers/ngmodal.js
@@ -9,14 +9,12 @@ define(['./module', 'darkwallet', 'frontend/port'], function (controllers, DarkW
   $scope.vars = $scope.modals.vars;
 
   $scope.ok = function(data) {
-      $scope.modals.page = false;
       $scope.modals.show = false;
       if ($scope.modals.okCallback) {
           $scope.modals.okCallback(data, $scope.vars);
       }
   }
   $scope.cancel = function(reason) {
-      $scope.modals.page = false;
       $scope.modals.show = false;
       if ($scope.modals.cancelCallback) {
           $scope.modals.cancelCallback(reason, $scope.vars);
