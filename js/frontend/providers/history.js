2c9d7b292fe076eef1033269ccddf0035d8a517e
Pablo Martin (wdp) <caedes@sindominio.net>
2014-06-21 11:58:18 +0200

    Show the right selected fund.

--- js/frontend/providers/history.js
+++ js/frontend/providers/history.js
@@ -120,6 +120,7 @@ function (providers, BtcUtils, DarkWallet, MultisigFund) {
       this.pocket.fund = new MultisigFund(fund);
       this.pocket.tasks = this.pocket.fund.tasks;
       this.pocket.isFund = true;
+      this.selectedPocket = type+':' + fundIndex;
   };
 
   HistoryProvider.prototype.selectAll = function() {
