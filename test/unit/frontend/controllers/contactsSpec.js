d172df11297d86f15b819e6dfd0b5c12b90cb5a0
Pablo Martin (wdp) <caedes@sindominio.net>
2014-06-23 00:50:16 +0200

    Add missing mock function.

--- test/unit/frontend/controllers/contactsSpec.js
+++ test/unit/frontend/controllers/contactsSpec.js
@@ -61,7 +61,7 @@ define(['angular-mocks', 'testUtils'], function (mocks, testUtils) {
             location._path = path;
           }
         };
-        var history = {refreshAddresses: function() {}};
+        var history = {refreshAddresses: function() {}, clearRowContacts: function() {}};
         var route = {contactId: undefined, current: {templateUrl: "bla"}};
         var wallet = {addToScope: function() {}};
         contactsController = $controller('ContactsCtrl', {$scope: scope, $routeParams: routeParams, $location: location, $route: route, $wallet: wallet, watch: watch, $history: history, notify: notify});
