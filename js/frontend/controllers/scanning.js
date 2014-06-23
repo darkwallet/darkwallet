e9aa47f9a6fd8d73dfad2af528cb5a66fc3cf570
Pablo Martin (wdp) <caedes@sindominio.net>
2014-06-23 02:36:49 +0200

    Change the scanner to generate all addresses till the ones found leaving no holes.

--- js/frontend/controllers/scanning.js
+++ js/frontend/controllers/scanning.js
@@ -12,20 +12,34 @@ define(['./module', 'darkwallet', 'util/scanner'], function (controllers, DarkWa
   var createAddresses = function(results) {
       var identity = DarkWallet.getIdentity();
       var pockets = identity.wallet.pockets;
+      var maxIndex = {};
+      // Initialize pockets and check last index for each pocket
       results.forEach(function(seq) {
           var pocketIndex = Math.floor(seq[0]/2);
+          if (!maxIndex.hasOwnProperty(seq[0])) {
+              maxIndex[seq[0]] = seq[1];
+          } else {
+              maxIndex[seq[0]] = Math.max(seq[1], maxIndex[seq[0]]);
+          }
           if (!pockets.hdPockets[pocketIndex]) {
               // Manual initialization of specific pocket
               pockets.hdPockets[pocketIndex] = {name: "Pocket " + pocketIndex};
               pockets.initPocketWallet(pocketIndex, 'hd', pockets.hdPockets[pocketIndex]);
-              pockets.store.save();
-          }
-          if (!identity.wallet.pubKeys[seq]) {
-              $wallet.generateAddress(seq[0], seq[1]);
-          } else {
-              console.log("Already exists!");
           }
       });
+      // Now generate addresses
+      Object.keys(maxIndex).forEach(function(branchId) {
+          var pocketIndex = Math.floor(branchId/2);
+          for(var i=0; i<=maxIndex[branchId]; i++) {
+              var seq = [branchId, i];
+              if (!identity.wallet.pubKeys[seq]) {
+                  $wallet.generateAddress(seq[0], seq[1]);
+              } else {
+                  console.log("Already exists!");
+              }
+          }
+      });
+      pockets.store.save();
   };
 
   // Update every time we get results for an address
