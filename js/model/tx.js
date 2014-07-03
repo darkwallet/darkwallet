791e3c5359d6534e59476d866d4a22702a490556
Pablo Martin (wdp) <caedes@sindominio.net>
2014-07-03 12:45:54 +0200

    Fix incorrect check in signMyInputs.

--- js/model/tx.js
+++ js/model/tx.js
@@ -173,7 +173,7 @@ Transaction.prototype.signMyInputs = function(inputs, newTx, privKeys) {
     for(var i=0; i<newTx.ins.length; i++) {
         var anIn = newTx.ins[i];
         var found = inputs.filter(function(myIn) {
-            return (myIn.hash === anIn.hash) && (myIn.index === anIn.index);
+            return (myIn.outpoint.hash === anIn.outpoin.hash) && (myIn.outpoint.index === anIn.outpoint.index);
         });
         if (found.length === 0) {
             continue;
