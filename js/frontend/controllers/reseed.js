3622602d4906c394aeb2c0db21063a2b3c39ecfe
Pablo Martin <caedes@sindominio.net>
2014-11-02 17:00:55 +0100

    Allow reseeding from gui, use with caution for now.

--- js/frontend/controllers/reseed.js
+++ js/frontend/controllers/reseed.js
@@ -4,14 +4,11 @@ define(['./module', 'darkwallet', 'model/upgrade'], function (controllers, DarkW
   controllers.controller('ReseedCtrl', ['$scope', 'modals', 'notify', '_Filter', function($scope, modals, notify, _) {
     $scope.reseedIdentity = function() {
         var identity = DarkWallet.getIdentity();
-        modals.password('Write your password to reseed', function(password) {
-            notify.warning('Not reseeding yet');
-            $scope.clearAlert();
-            return;
+        modals.password('Write your password to reseed. EXPERIMENTAL. You can lose all data.', function(password) {
             if (Upgrade(identity.store.store, identity, password)) {
                 if (identity.store.get('version') === 5) {
                     identity.store.save();
-                    notify.warning('Reseed successfull ' + identity.store.get('version'));
+                    notify.warning('Reseed successfull');
                     $scope.clearAlert();
                 } else {
                     notify.warning('Could not finish reseeding');
