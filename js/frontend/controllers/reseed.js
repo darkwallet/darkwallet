8c8a2019e310cc755c61df8396703e6d0c60f81a
Pablo Martin <caedes@sindominio.net>
2014-11-05 07:02:21 +0100

    Add transation to reseed.

--- js/frontend/controllers/reseed.js
+++ js/frontend/controllers/reseed.js
@@ -3,7 +3,7 @@
 define(['./module', 'darkwallet', 'model/upgrade'], function (controllers, DarkWallet, Upgrade) {
   controllers.controller('ReseedCtrl', ['$scope', 'modals', 'notify', '$wallet', '_Filter', function($scope, modals, notify, $wallet, _) {
     $scope.reseedIdentity = function() {
-        modals.password('Write your password to reseed.', function(password) {
+        modals.password(_('Write your password to reseed.'), function(password) {
             var identity = DarkWallet.getIdentity();
             try {
                 var res = Upgrade(identity.store.store, identity, password);
@@ -13,7 +13,7 @@ define(['./module', 'darkwallet', 'model/upgrade'], function (controllers, DarkW
                 } else {
                     console.log(e.stack);
                     console.log(e);
-                    notify.error('Fatal error!', e.message);
+                    notify.error(_('Fatal error!'), e.message);
                 }
                 return;
             }
@@ -21,13 +21,13 @@ define(['./module', 'darkwallet', 'model/upgrade'], function (controllers, DarkW
                 if (identity.store.get('version') === 5) {
                     identity.store.save();
                     $wallet.onIdentityLoaded(identity);
-                    notify.success('Reseed successfull');
+                    notify.success(_('Reseed successfull'));
                     $scope.clearAlert();
                 } else {
-                    notify.warning('Could not finish reseeding');
+                    notify.warning(_('Could not finish reseeding'));
                 }
             } else {
-                notify.warning('Could not reseed');
+                notify.warning(_('Could not reseed'));
             }
         });
     };
