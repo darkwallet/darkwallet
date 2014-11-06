287d2ae07272695bc5f17b63775255513369beb5
Sem <sembrestels@riseup.net>
2014-11-06 04:41:50 +0100

    Added transifex translations

--- i18n/_index.js
+++ i18n/_index.js
@@ -3,12 +3,31 @@
 define(function() {
     var lang = [
         {"name": "English (US)", "code": "en_US"},
-        {"name": "Español (España)", "code": "es_ES"}
+        {"name": "Español (España)", "code": "es_ES"},
+        {"name": "Français", "code": "fr"},
+        {"name": "Nederlands", "code": "nl"},
+        {"name": "Català", "code": "ca"},
+        {"name": "Português (Brasil)", "code": "pt_BR"},
+        {"name": "Norsk", "code": "no"},
+        {"name": "Português", "code": "pt_PT"},
+        {"name": "Deutsch", "code": "de"},
+        {"name": "Italiano", "code": "it_IT"},
+        {"name": "Magyar", "code": "hu"},
+        {"name": "Slovenčina", "code": "sk"},
+        {"name": "Čeština", "code": "cs"},
+        {"name": "Română", "code": "ro"},
+        {"name": "Nederlands (België)", "code": "nl_BE"},
+        {"name": "日本語", "code": "ja"},
+        {"name": "English (UK)", "code": "en_GB"},
+        {"name": "עִבְרִית", "code": "he"}
     ];
     
     var fallbacks = {
         "en": "en_US",
-        "es": "es_ES"
+        "es": "es_ES",
+        "it": "it_IT",
+        "nl": "nl_BE",
+        "pt": "pt_PT"
     };
     
     Object.defineProperty(lang, 'preferedLanguage', {
