03690a9b0c64f15fee24dcaeb8a34a7803fdfd8c
Pablo Martin (wdp) <caedes@sindominio.net>
2015-01-22 21:59:07 +0100

    Don't add the listener if we have no body.

--- src/js/frontend/scripts/inject.js
+++ src/js/frontend/scripts/inject.js
@@ -24,14 +24,16 @@ var bitcoinUri = function(uri) {
   return false;
 };
 
-document.body.addEventListener('click', function(e) {
-  var elem = e.target;
-  while (elem && elem.tagName != 'A') {
-    elem = elem.parentNode;
-  }
-  if (elem && elem.tagName == 'A' && bitcoinUri(elem.href)) {
-    var u = bitcoinUri(elem.href);
-    chrome.runtime.sendMessage({ type: 'newTab', url: u });
-    e.preventDefault();
-  }
-}, false);
+if (document.body) {
+  document.body.addEventListener('click', function(e) {
+    var elem = e.target;
+    while (elem && elem.tagName != 'A') {
+      elem = elem.parentNode;
+    }
+    if (elem && elem.tagName == 'A' && bitcoinUri(elem.href)) {
+      var u = bitcoinUri(elem.href);
+      chrome.runtime.sendMessage({ type: 'newTab', url: u });
+      e.preventDefault();
+    }
+  }, false);
+}
