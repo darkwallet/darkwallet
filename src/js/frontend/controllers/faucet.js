29770f27e9c3a663c2803ce26ccbedb62db19cdc
Pablo Martin (wdp) <caedes@sindominio.net>
2015-01-25 19:50:47 +0100

    Disable faucet buttons when sending or no longer available.

--- src/js/frontend/controllers/faucet.js
+++ src/js/frontend/controllers/faucet.js
@@ -6,20 +6,26 @@ define(['./module', 'darkwallet'], function (controllers, DarkWallet) {
 
     var day = 60*60*24*1000;
 
+    $scope.faucetRunning = {haskoin: false, helloblock: false};
+
     $scope.pokeHelloblockFaucet = function() {
         var walletAddress = $wallet.generateAddress();
         walletAddress.label = 'helloblock';
+        $scope.faucetRunning.helloblock = true;
         $http({method: 'POST', url: "https://testnet.helloblock.io/v1/faucet/withdrawal", data: "toAddress="+walletAddress.address+"&value=1000000", headers:{'Content-Type': 'application/x-www-form-urlencoded'}})
            .success(function(data) {
                notify.success(_("Coins requested!"));
+               $scope.faucetRunning.helloblock = false;
            })
            .error(function() {
                notify.warning(_("Error fetching coins"));
+               $scope.faucetRunning.helloblock = false;
            });
 
     };
 
     $scope.pokeHaskoinFaucet = function() {
+        $scope.faucetRunning.haskoin = true;
         if (identity.settings.faucetTimestamp && (identity.settings.faucetTimestamp > Date.now() - day)) {
             notify.warning(_("You can only use the haskoin faucet one time a day!"));
             return;
